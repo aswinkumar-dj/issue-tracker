dfe0de4d260030b2e71d5ec1691ec42c88124820
aswinkumar-dj <203110173+aswinkumar-dj@users.noreply.github.com>
2025-12-14 19:40:24 +0530

    Setting Prisma ORM  v5

---
+++ app/api/issues/route.ts
@@ -0,0 +1,25 @@
+import { prisma } from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+const createIssueSchema = z.object({
+  name: z.string().min(1).max(255),
+  description: z.string().min(1),
+});
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+
+  const validation = createIssueSchema.safeParse(body);
+  if (!validation.success) {
+    return NextResponse.json(validation.error.format(), { status: 400 });
+  }
+
+  const { name, description } = validation.data;
+
+  const newIssue = await prisma.issues.create({
+    data: { name, description },
+  });
+
+  return NextResponse.json(newIssue, { status: 201 });
+}
