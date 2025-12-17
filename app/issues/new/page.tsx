5b20fd0ffd95200e4bf9b580ae820acbcf4bb985
aswinkumar-dj <203110173+aswinkumar-dj@users.noreply.github.com>
2025-12-17 18:01:06 +0530

    Handle Errors

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 import dynamic from "next/dynamic";
-import { Button, TextField } from "@radix-ui/themes";
+import { Button, Callout, TextField } from "@radix-ui/themes";
 import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 const MDEditor = dynamic(() => import("@uiw/react-md-editor"), {
   ssr: false,
@@ -17,36 +18,49 @@ interface IssueForm {
 
 const NewIssuePage = () => {
   const { register, control, handleSubmit } = useForm<IssueForm>();
+  const [error, setError] = useState("");
   const router = useRouter();
 
   return (
-    <form
-      className="max-w-xl space-y-3"
-      onSubmit={handleSubmit(async (data) => {
-        await axios.post("/api/issues", data);
-        router.push("/issues");
-      })}
-    >
-      <TextField.Root placeholder="Title" {...register("name")} />
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        className=" space-y-3"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post("/api/issues", data);
+            router.push("/issues");
+          } catch (error) {
+            console.log(error);
+            setError("An unexpected error occured.");
+          }
+        })}
+      >
+        <TextField.Root placeholder="Title" {...register("name")} />
 
-      <Controller
-        name="description"
-        control={control}
-        render={({ field }) => (
-          <div data-color-mode="light">
-            <MDEditor
-              value={field.value}
-              onChange={(val) => field.onChange(val ?? "")}
-              onBlur={field.onBlur}
-              preview="edit"
-              height={200}
-            />
-          </div>
-        )}
-      />
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <div data-color-mode="light">
+              <MDEditor
+                value={field.value}
+                onChange={(val) => field.onChange(val ?? "")}
+                onBlur={field.onBlur}
+                preview="edit"
+                height={200}
+              />
+            </div>
+          )}
+        />
 
-      <Button>Submit New Issue</Button>
-    </form>
+        <Button>Submit New Issue</Button>
+      </form>
+    </div>
   );
 };
 
